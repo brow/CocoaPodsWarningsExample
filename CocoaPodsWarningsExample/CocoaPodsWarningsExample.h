acb73e04f2da89e3486ff022fc86c76c401f8793
Tom Brow <tom@tombrow.com>
2013-10-14 15:26:34 -0500

    fresh Xcode project

---
+++ CocoaPodsWarningsExample/CocoaPodsWarningsExample.h
@@ -0,0 +1,13 @@
+//
+//  CocoaPodsWarningsExample.h
+//  CocoaPodsWarningsExample
+//
+//  Created by Tom Brow on 10/14/13.
+//  Copyright (c) 2013 Example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodsWarningsExample : NSObject
+
+@end
---
+++ CocoaPodsWarningsExample/CocoaPodsWarningsExample.m
@@ -0,0 +1,13 @@
+//
+//  CocoaPodsWarningsExample.m
+//  CocoaPodsWarningsExample
+//
+//  Created by Tom Brow on 10/14/13.
+//  Copyright (c) 2013 Example. All rights reserved.
+//
+
+#import "CocoaPodsWarningsExample.h"
+
+@implementation CocoaPodsWarningsExample
+
+@end
